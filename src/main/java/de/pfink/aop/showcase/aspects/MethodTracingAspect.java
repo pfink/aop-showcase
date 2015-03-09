12a842fbdbd83c75d7591c66614dd3925b60e0fd
Patrick Fink <dhbw@pfink.de>
2015-03-09 03:04:06 +0100

    Adjust MethodTracingAspect implementation

--- src/main/java/de/pfink/aop/showcase/aspects/MethodTracingAspect.java
+++ src/main/java/de/pfink/aop/showcase/aspects/MethodTracingAspect.java
@@ -1,11 +1,11 @@
 package de.pfink.aop.showcase.aspects;
 
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import lombok.extern.java.Log;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 /**
@@ -15,16 +15,16 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 @Log
 public class MethodTracingAspect {
-
     @Pointcut("execution(* *(..))")
-    public void call() {}
+    public void allMethodExecutions() {}
 
-    @Around("de.pfink.aop.showcase.aspects.MethodTracingAspect.call()")
-    public Object myTrace(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Before("de.pfink.aop.showcase.aspects.MethodTracingAspect.allMethodExecutions()")
+    public void traceOnMethodBegin(JoinPoint joinPoint) {
         log.log(Level.INFO, "Triggered ''{0}'' method!", joinPoint.getSignature().getName());        
-        Object retVal = joinPoint.proceed();            
-        log.log(Level.INFO, "Finished ''{0}'' method successfully!", joinPoint.getSignature().getName());        
-        return retVal;
     }
-
+    
+    @After("de.pfink.aop.showcase.aspects.MethodTracingAspect.allMethodExecutions()")
+    public void traceOnMethodEnd(JoinPoint joinPoint) {
+        log.log(Level.INFO, "Finished ''{0}'' method successfully!", joinPoint.getSignature().getName());     
+    }
 }
